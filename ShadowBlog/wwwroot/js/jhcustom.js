7e75c6d2df151618241de8a6226d841c344afcbb
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-12-20 21:27:05 -0500

    Production application

--- ShadowBlog/wwwroot/js/jhcustom.js
+++ ShadowBlog/wwwroot/js/jhcustom.js
@@ -6,3 +6,15 @@ owl.owlCarousel({
     autoplayTimeout: 5000,
     autoplayHoverPause: false
 });
+
+//////*Start Custom Sidenav*//////
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/////*End Custom Sidenav*/////
